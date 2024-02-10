348cf3e66e45a88fdcc5c1148679ba3012c8b135
Alvin Liang <alvinliang@Alvins-MacBook-Air.local>
2024-02-10 11:10:37 -0500

    block hashing and chaining preliminary implementaion

--- block.go
+++ block.go
@@ -1,23 +1,41 @@
 package main
 
 import (
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"strings"
 	"time"
+
+	"golang.org/x/crypto/sha3"
 )
 
 type Block struct {
 	nonce        int
 	prevHash     [32]byte
 	timestamp    int64
-	transactions []string
+	transactions []*Transaction
 }
 
+func (b *Block) Hash() [32]byte {
+	m, _ := json.Marshal(b)
+	return sha3.Sum256(m)
+}
+
+func NewBlock(nonce int, prevHash [32]byte) *Block {
+	return &Block{
+		timestamp: time.Now().UnixNano(),
+		nonce:     nonce,
+		prevHash:  prevHash,
+	}
+}
+
+// Stringify: Stringer interface implementation
 func (b Block) String() string {
 	var sb strings.Builder
 
 	sb.WriteString(fmt.Sprintf("\nNonce: %d\n", b.nonce))
-	sb.WriteString(fmt.Sprintf("PrevHash: %s\n", b.prevHash))
+	sb.WriteString(fmt.Sprintf("PrevHash: %s\n", hex.EncodeToString(b.prevHash[:])))
 	sb.WriteString(fmt.Sprintf("Timestamp: %d\n", b.timestamp))
 	sb.WriteString("Transactions:\n")
 
@@ -29,10 +47,18 @@ func (b Block) String() string {
 	return sb.String()
 }
 
-func NewBlock(nonce int, prevHash [32]byte) *Block {
-	return &Block{
-		timestamp: time.Now().UnixNano(),
-		nonce:     nonce,
-		prevHash:  prevHash,
-	}
+// JSON Encoding: this implements the json.Marshaller interface
+// MarshalJSON needs to overriden as json.Marshal does not encode private fields
+func (b *Block) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct {
+		Nonce        int           `json:"nonce"`
+		PrevHash     [32]byte      `json:"previous_hash"`
+		Timestamp    int64         `json:"timestamp"`
+		Transactions []Transaction `json:"transactions"`
+	}{
+		Timestamp:    b.timestamp,
+		Nonce:        b.nonce,
+		PrevHash:     b.prevHash,
+		Transactions: b.transactions,
+	})
 }
--- blockchain.go
+++ blockchain.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"strings"
 	"time"
-	"golang.org/x/crypto/sha3"
 )
 
 func init() {
@@ -13,20 +12,25 @@ func init() {
 }
 
 func main() {
-	bc := NewBlockChain()
-	fmt.Println(bc)
+	// bc := NewBlockChain()
+	// bc.CreateBlock(1, bc.hashPrevBlock())
+	// bc.CreateBlock(5, bc.hashPrevBlock())
+	// fmt.Println(bc)
+	sender := new([20]byte)
+	receiver := new([20]byte)
+	t := NewTransaction(*sender, *receiver, 8)
+	fmt.Println(t)
 }
 
 type BlockChain struct {
-	memPool []Transaction
+	memPool []*Transaction
 	chain   []*Block
 }
 
 func NewBlockChain() *BlockChain {
 	bc := new(BlockChain)
-	//CreateBlock appends to the chain
-	sum := sha3.Sum256([]byte("Genesis"))
-	bc.CreateBlock(0, sum)
+	//Generates an Empty Block as the prevHash of the Genesis Block
+	bc.CreateBlock(0, (&Block{}).Hash())
 	return bc
 }
 
@@ -38,8 +42,16 @@ func (bc *BlockChain) CreateBlock(nonce int, previousHash [32]byte) *Block {
 	return b
 }
 
-//Formatting methods
+// Helper Methods
+func (bc *BlockChain) LastBlock() *Block {
+	return bc.chain[len(bc.chain)-1]
+}
 
+func (bc *BlockChain) hashPrevBlock() [32]byte {
+	return bc.LastBlock().Hash()
+}
+
+// Formatting methods
 func (bc *BlockChain) String() string {
 	var sb strings.Builder
 	div := strings.Repeat("=", 25)
